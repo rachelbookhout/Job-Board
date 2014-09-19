35d6dea7395a086745670ec5c0adc0c3091474a5
rachelbookhout <rbookhou@gmail.com>
2014-09-18 21:13:48 -0400

    Working Selection

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -7,4 +7,38 @@ class CompaniesController < ApplicationController
     @company = Company.find(params[:id])
     @jobs = @company.jobs
   end
+
+  def new
+  @company = Company.new
+  end
+
+  def create
+  @company = Company.new(company_params)
+  if @company.save
+    redirect_to @company
+  else
+   render 'new'
+  end
+  end
+
+  def edit
+  @company = Company.find(params[:id])
+  end
+
+  def update
+  @company = Company.find(params[:id])
+  if @company.update(company_params)
+    redirect_to @company
+  else
+   render 'edit'
+  end
+  end
+
+
+
+private
+
+def company_params
+  params.require(:company).permit(:name,:description,:website,:location)
+end
 end
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -8,5 +8,25 @@ class JobsController < ApplicationController
     @job= Job.find(params[:id])
   end
 
+  def create
+    @job = Job.new(job_params)
+
+    if @job.save
+      redirect_to @job
+    else
+      render :new
+    end
+  end
+
+  def new
+    @job = Job.new
+    @companies = Company.all
+  end
+
+private
+
+def job_params
+  params.require(:job).permit(:title,:description,:company_id)
+end
 
 end
